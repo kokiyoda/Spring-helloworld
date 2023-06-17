65d93f32a149103e24387eb1a418499ba1559ddf
koki yoda <134734832+kokiyoda@users.noreply.github.com>
2023-06-18 08:18:07 +0900

    HelloWorldをlocalTimeの取得に変更

--- src/main/java/com/koki/helloworld/HelloworldController.java
+++ src/main/java/com/koki/helloworld/HelloworldController.java
@@ -3,13 +3,21 @@ package com.koki.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 @RestController
 
 public class HelloworldController {
 
-        @GetMapping("/hello")
-        public String hello () {
-        return "Hello World";
+    @GetMapping("/localTime")
+    public String localTime() {
+        LocalDateTime localTime = LocalDateTime.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String formatted = dateFormat.format(localTime);
+        return "Hello current time " + formatted;
+
     }
 
 }
